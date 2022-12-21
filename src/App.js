9c1e195a815a09992b85c64dd22cbb8007e416b8
Allyson Alves <allysonalves.01@gmail.com>
2022-12-21 15:43:35 -0300

    Fazendo um site react usando html

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import myimg from './assets/foto-digital.png';
+import './styles.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="login__container">
+      <div className="img__container">
+        <img src={myimg} className="img" alt="pessoa no notebook com uma digital sobre a imagem" />
+      </div>
+      <form method="post" className="login__form">
+        <div className="form__content">
+          <h2 className="login__title">Login</h2>
+          <div className="user__inputs">
+            <div>
+              <h3>Email</h3>
+              <input placeholder="Digite seu email" text="Email" type="text" className="user__input" />
+            </div>
+            <div>
+              <h3>Senha</h3>
+              <input placeholder="Digite sua senha" text="Senha" type="password" className="user__input" />
+            </div>
+            <button type="submit" className="button">
+              Acessar
+            </button>
+          </div>
+        </div>
+      </form>
+    </main >
   );
 }
 
